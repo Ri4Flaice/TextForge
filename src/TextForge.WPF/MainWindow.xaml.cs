e065e019e3a188a2eeb9b37d5231d397f0c3e610
Damir Ramazanov <chikibambonika007@gmail.com>
2024-05-29 16:58:28 +0500

    Added architecture, stylecop

---
+++ src/TextForge.WPF/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace TextForge.WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    /// <summary>
+    /// Initializes a new instance of the class <see cref="MainWindow"/>.
+    /// </summary>
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
